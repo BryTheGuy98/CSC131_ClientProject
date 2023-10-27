a538d17bebca93063ca560fc302bf11f6c9327e3
AkalUstat <akalustat.singh@gmail.com>
2023-10-27 16:14:21 -0700

    Working templating

--- functions/index.js
+++ functions/index.js
@@ -1,42 +1,18 @@
 // eslint-disable-next-line no-unused-vars
 const { Timestamp } = require( "firebase-admin/firestore" );
-/**
- * Define the Item type for JSDoc
- * @typedef {Object} Item
- * @property {string} description
- * @property {number} quantity
- * @property {number} unitPrice
- *
- */
-
-/**
- * Define the Document properties for JSDoc
- * @typedef {Object} Data
- * @property {string} clientName
- * @property {string} clientPO
- * @property {number} clientID
- * @property {string} paymentTerms
- * @property {string} termsAndConditions
- * @property {Timestamp} dueDate
- * @property {Timestamp} invoiceDate
- * @property {string} invoiceNumber
- * @property {string} description
- * @property {Item[]} items
- * @property {string} reference
- * @property {boolean} runPDF
- */
-
 const { initializeApp } = require( "firebase-admin/app" );
 const { error } = require( "firebase-functions/logger" );
 const { onDocumentWritten } = require( "firebase-functions/v2/firestore" );
 const { getStorage } = require( "firebase-admin/storage" );
 const { setGlobalOptions } = require( "firebase-functions/v2" );
 
-const { writeFile } = require( "fs/promises" );
+const { writeFile, readdir } = require( "fs/promises" );
 const os = require( "os" );
 
 const tectonic = require( "tectonic-js" );
-const nodemailer = require( "nodemailer" );
+const { Template } = require( "./lib/templating" );
+const { log } = require( "console" );
+// const nodemailer = require( "nodemailer" );
 
 setGlobalOptions(
     {
@@ -45,62 +21,6 @@ setGlobalOptions(
     } );
 
 initializeApp();
-
-/**
- * Iterate over the items property in a document to generate a latex table
- * @param {Item[]} items
- * @return {String} a String
- */
-function createTable( items ) {
-  let str = "\\begin{invoicetable}\n";
-  items.forEach( ( item ) => {
-    str += `\\invoiceitem{${item.description}}{${item.quantity}}{${item.unitPrice}}{}\n`;
-  } );
-
-  str += "\\end{invoicetable}";
-
-  return str;
-}
-
-/**
- * Creates the latex file to compile.
- * Does regex replaces for dynamic data and then writes it to a file in the tmpdir
- * @param {Data} data Take in the document data from Firestore
- * @param {Buffer} buffr This is the template buffer downloaded from Cloud Bucket
- * @return {number} The invoice number
- */
-async function createLatex( data, buffr ) {
-  const { clientName,
-    clientPO,
-    clientID,
-    dueDate,
-    termsAndConditions,
-    invoiceDate,
-    invoiceNumber,
-    description,
-    items,
-    reference } = data;
-  const dataTable = createTable( items );
-
-  const dataToRegexMap = [
-    [ clientName, /{{clientName}}/ ],
-    [ clientPO, /{{clientPO}}/ ],
-    [ clientID, /{{clientID}}/ ],
-    [ dueDate, /{{dueDate}}/ ],
-    [ invoiceDate.toDate().toDateString(), /{{invoiceDate}}/ ],
-    [ invoiceNumber, /{{invoiceNumber}}/ ],
-    [ description, /{{description}}/ ],
-    [ dataTable, /{{invoiceTable}}/ ],
-    [ termsAndConditions, /{{terms}}/ ],
-    [ reference, /{{reference}}/ ] ];
-
-  const substitutedString = dataToRegexMap.reduce(
-      ( oldStr, [ data, regex ] ) => oldStr.replace( regex, data ),
-      buffr.toString() );
-
-  await writeFile( os.tmpdir + `/invoice_${invoiceNumber}.tex`, substitutedString, { encoding: "utf-8", flag: "w" } );
-  return invoiceNumber;
-}
 
 exports.onRunPDF = onDocumentWritten( "Invoice/{invoidId}", async ( event ) => {
   const previousDocument = event.data.before;
@@ -117,22 +37,46 @@ exports.onRunPDF = onDocumentWritten( "Invoice/{invoidId}", async ( event ) => {
   const storage = getStorage();
   const bucket = storage.bucket();
 
-  try {
-    const { folder, assets, main } = currentData.template;
+  const { folder, main } = currentData.template;
 
+  try {
     const texBuffer = await bucket
-        .file( `templates/${folder}/${main}` )
+        .file( `templates/${folder}/template.tex` )
         .download();
 
-    await Promise.all(
-        assets.map(
-            ( asset ) => async () => bucket.file( `templates/${folder}/${asset}` ).download(),
-        ),
-    );
 
-    const invoiceNumber = await createLatex( currentData, texBuffer );
+    // see https://cloud.google.com/storage/docs/samples/storage-list-files-with-prefix
+    // get our assets stored in {templateFolder}/assets/
+    await bucket.getFiles(
+        { prefix: `templates/${folder}/assets/`, delimiter: "/" },
+        ( err, files ) => {
+          files
+              .filter(
+                  ( file ) =>
+                  // this function also lists folders, so filter them out
+                    file.name.includes( "." ) )
+              .forEach( ( file ) => {
+              // then download the files
+
+                // cloud storage name contains the path, so strip it out
+                const name = file.name.substring(
+                    file.name.lastIndexOf( "/" ) );
+                file.download( { destination: os.tmpdir + name } );
+              } );
+        } );
+
+
+    const invoiceNumber = currentData.invoiceNumber;
     const fileName = `invoice_${invoiceNumber}`;
+
+    // create our final latex file
+    const newTex = new Template( texBuffer ).substitute( currentData );
+    await writeFile( os.tmpdir + `/${fileName}.tex`, newTex, { encoding: "utf-8", flag: "w" } );
+
+    // process and compile it
     await tectonic( os.tmpdir + `/${fileName}.tex -o ` + os.tmpdir );
+
+    // and then save it to the invoices folder
     await bucket.upload( os.tmpdir + `/${fileName}.pdf`, { destination: `invoices/${fileName}.pdf` } );
   } catch ( e ) {
     error( e );
@@ -146,54 +90,54 @@ exports.onRunPDF = onDocumentWritten( "Invoice/{invoidId}", async ( event ) => {
 } );
 
 
-const mailTransport = nodemailer.createTransport( {
-  service: "gmail",
-  auth: {
-    user: `${process.env.USER_EMAIL}`,
-    pass: `${process.env.USER_PASS}`,
-  },
-} );
-exports.sendEmail = onDocumentWritten( "Invoice/{invoidId}", async ( event ) => {
-  const previousDocument = event.data.before;
-  const currentDocument = event.data.after;
+// const mailTransport = nodemailer.createTransport( {
+//   service: "gmail",
+//   auth: {
+//     user: `${process.env.USER_EMAIL}`,
+//     pass: `${process.env.USER_PASS}`,
+//   },
+// } );
+// exports.sendEmail = onDocumentWritten( "Invoice/{invoidId}", async ( event ) => {
+//   const previousDocument = event.data.before;
+//   const currentDocument = event.data.after;
 
-  const previousData = previousDocument.data();
-  const currentData = currentDocument.data();
+//   const previousData = previousDocument.data();
+//   const currentData = currentDocument.data();
 
 
-  if ( !currentDocument.data().sendEmail ||
-      !currentDocument.exists ||
-  ( previousDocument.exists && previousData.sendEmail === currentData.sendEmail ) ) {
-    return null;
-  }
+//   if ( !currentDocument.data().sendEmail ||
+//       !currentDocument.exists ||
+//   ( previousDocument.exists && previousData.sendEmail === currentData.sendEmail ) ) {
+//     return null;
+//   }
 
-  const fileName = `invoice_${currentData.invoiceNumber}.pdf`;
-  const downloadPath = `${os.tmpdir}/${fileName}`;
+//   const fileName = `invoice_${currentData.invoiceNumber}.pdf`;
+//   const downloadPath = `${os.tmpdir}/${fileName}`;
 
-  const storage = getStorage();
-  const bucket = storage.bucket();
+//   const storage = getStorage();
+//   const bucket = storage.bucket();
 
-  await bucket.file( `invoices/${fileName}` ).download( { destination: downloadPath } );
+//   await bucket.file( `invoices/${fileName}` ).download( { destination: downloadPath } );
 
-  const mailOpts = {
-    from: `DevWave ${process.env.USER_EMAIL}`,
-    to: currentData.clientEmail,
-    subject: `TESTING FIREBASE: Your Invoice for Order ${currentData.invoiceNumber} from Ansync, INC`,
-    attachments: [
-      {
-        filename: fileName,
-        path: downloadPath,
-      },
-    ],
-  };
+//   const mailOpts = {
+//     from: `DevWave ${process.env.USER_EMAIL}`,
+//     to: currentData.clientEmail,
+//     subject: `TESTING FIREBASE: Your Invoice for Order ${currentData.invoiceNumber} from Ansync, INC`,
+//     attachments: [
+//       {
+//         filename: fileName,
+//         path: downloadPath,
+//       },
+//     ],
+//   };
 
-  try {
-    await mailTransport.sendMail( mailOpts );
-  } catch ( e ) {
-    error( e );
-  }
-  return event.data.after.ref.update( {
-    sendEmail: false,
-  } );
-} );
+//   try {
+//     await mailTransport.sendMail( mailOpts );
+//   } catch ( e ) {
+//     error( e );
+//   }
+//   return event.data.after.ref.update( {
+//     sendEmail: false,
+//   } );
+// } );
 
