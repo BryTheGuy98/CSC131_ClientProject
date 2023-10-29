b641967af584a2e7cfa829e326da59af818690a5
Akal-Ustat Singh <akalustat.singh@gmail.com>
2023-10-28 17:05:26 -0700

    Setup Templating for interacting with LaTeX (#4)
    * Working templating
    
    * missing things from original commit
    
    * Document templating class
    
    * Uncomment mailer function

--- functions/index.js
+++ functions/index.js
@@ -1,41 +1,14 @@
 // eslint-disable-next-line no-unused-vars
-const { Timestamp } = require( "firebase-admin/firestore" );
-/**
- * Define the Item type for JSDoc
- * @typedef {Object} Item
- * @property {string} description
- * @property {number} quantity
- * @property {number} unitPrice
- *
- */
-
-/**
- * Define the Document properties for JSDoc
- * @typedef {Object} Data
- * @property {string} clientName
- * @property {string} clientPO
- * @property {number} clientID
- * @property {string} paymentTerms
- * @property {string} termsAndConditions
- * @property {Timestamp} dueDate
- * @property {Timestamp} invoiceDate
- * @property {string} invoiceNumber
- * @property {string} description
- * @property {Item[]} items
- * @property {string} reference
- * @property {boolean} runPDF
- */
-
 const { initializeApp } = require( "firebase-admin/app" );
 const { error } = require( "firebase-functions/logger" );
 const { onDocumentWritten } = require( "firebase-functions/v2/firestore" );
 const { getStorage } = require( "firebase-admin/storage" );
 const { setGlobalOptions } = require( "firebase-functions/v2" );
 
-const { writeFile } = require( "fs/promises" );
 const os = require( "os" );
 
 const tectonic = require( "tectonic-js" );
+const { Template } = require( "./lib/templating" );
 const nodemailer = require( "nodemailer" );
 
 setGlobalOptions(
@@ -45,62 +18,6 @@ setGlobalOptions(
     } );
 
 initializeApp();
-
-/**
- * Iterate over the items property in a document to generate a latex table
- * @param {Item[]} items
- * @return {String} a String
- */
-function createTable( items ) {
-  let str = "\\begin{invoicetable}\n";
-  items.forEach( ( item ) => {
-    str += `\\invoiceitem{${item.description}}{${item.quantity}}{${item.unitPrice}}{}\n`;
-  } );
-
-  str += "\\end{invoicetable}";
-
-  return str;
-}
-
-/**
- * Creates the latex file to compile.
- * Does regex replaces for dynamic data and then writes it to a file in the tmpdir
- * @param {Data} data Take in the document data from Firestore
- * @param {Buffer} buffr This is the template buffer downloaded from Cloud Bucket
- * @return {number} The invoice number
- */
-async function createLatex( data, buffr ) {
-  const { clientName,
-    clientPO,
-    clientID,
-    dueDate,
-    termsAndConditions,
-    invoiceDate,
-    invoiceNumber,
-    description,
-    items,
-    reference } = data;
-  const dataTable = createTable( items );
-
-  const dataToRegexMap = [
-    [ clientName, /{{clientName}}/ ],
-    [ clientPO, /{{clientPO}}/ ],
-    [ clientID, /{{clientID}}/ ],
-    [ dueDate, /{{dueDate}}/ ],
-    [ invoiceDate.toDate().toDateString(), /{{invoiceDate}}/ ],
-    [ invoiceNumber, /{{invoiceNumber}}/ ],
-    [ description, /{{description}}/ ],
-    [ dataTable, /{{invoiceTable}}/ ],
-    [ termsAndConditions, /{{terms}}/ ],
-    [ reference, /{{reference}}/ ] ];
-
-  const substitutedString = dataToRegexMap.reduce(
-      ( oldStr, [ data, regex ] ) => oldStr.replace( regex, data ),
-      buffr.toString() );
-
-  await writeFile( os.tmpdir + `/invoice_${invoiceNumber}.tex`, substitutedString, { encoding: "utf-8", flag: "w" } );
-  return invoiceNumber;
-}
 
 exports.onRunPDF = onDocumentWritten( "Invoice/{invoidId}", async ( event ) => {
   const previousDocument = event.data.before;
@@ -117,22 +34,55 @@ exports.onRunPDF = onDocumentWritten( "Invoice/{invoidId}", async ( event ) => {
   const storage = getStorage();
   const bucket = storage.bucket();
 
-  try {
-    const { folder, assets, main } = currentData.template;
+  const { folder, main } = currentData.template;
 
+  try {
     const texBuffer = await bucket
-        .file( `templates/${folder}/${main}` )
+        .file( `templates/${folder}/template.tex` )
         .download();
 
-    await Promise.all(
-        assets.map(
-            ( asset ) => async () => bucket.file( `templates/${folder}/${asset}` ).download(),
-        ),
-    );
 
-    const invoiceNumber = await createLatex( currentData, texBuffer );
+    // see https://cloud.google.com/storage/docs/samples/storage-list-files-with-prefix
+    // get our assets stored in {templateFolder}/assets/
+    await bucket.getFiles(
+        { prefix: `templates/${folder}/assets/`, delimiter: "/" },
+        ( err, files ) => {
+          files
+              .filter(
+                  ( file ) =>
+                  // this function also lists folders, so filter them out
+                    file.name.includes( "." ) )
+              .forEach( ( file ) => {
+              // then download the files
+
+                // cloud storage name contains the path, so strip it out
+                const name = file.name.substring(
+                    file.name.lastIndexOf( "/" ) );
+                file.download( { destination: os.tmpdir + name } );
+              } );
+        } );
+
+
+    const invoiceNumber = currentData.invoiceNumber;
     const fileName = `invoice_${invoiceNumber}`;
+
+    // create our final latex file
+    const templater = new Template( texBuffer );
+    try {
+      await templater
+          .substitute( currentData )
+          .writeToFile( os.tmpdir + "/" + fileName + ".tex", { encoding: "utf-8", flag: "w" } );
+    } catch ( e ) {
+      error( `
+        Something went wrong with templating. Please read the error.
+        ${e}
+      ` );
+    }
+
+    // process and compile it
     await tectonic( os.tmpdir + `/${fileName}.tex -o ` + os.tmpdir );
+
+    // and then save it to the invoices folder
     await bucket.upload( os.tmpdir + `/${fileName}.pdf`, { destination: `invoices/${fileName}.pdf` } );
   } catch ( e ) {
     error( e );
---
+++ functions/lib/typedef.js
@@ -0,0 +1,25 @@
+/**
+ * Define the Item type for JSDoc
+ * @typedef {Object} Item
+ * @property {string} description
+ * @property {number} quantity
+ * @property {number} unitPrice
+ *
+ */
+
+/**
+ * Define the Document properties for JSDoc
+ * @typedef {Object} Data
+ * @property {string} clientName
+ * @property {string} clientPO
+ * @property {number} clientID
+ * @property {string} paymentTerms
+ * @property {string} termsAndConditions
+ * @property {Timestamp} dueDate
+ * @property {Timestamp} invoiceDate
+ * @property {string} invoiceNumber
+ * @property {string} description
+ * @property {Item[]} items
+ * @property {string} reference
+ * @property {boolean} runPDF
+ */
